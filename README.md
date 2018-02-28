db8ef8f3190df39cc9da88b17b023762488ecddd
Eddie Jimenez <casadebanho@gmail.com>
2018-02-28 01:15:44 -0800

    added react rendering

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## Versioning
 Version | Update
 --- | --- |
+0.0.3 | Added react rendering
 0.0.2 | Added static layout
 0.0.1 | Initial commit
--- src/components/app.js
+++ src/components/app.js
@@ -1,29 +1,20 @@
 import React from 'react';
 
+const NUM_SPINNERS = 100;
+
 export default class App extends React.Component {
+  renderCircles = (count) => {
+    const margin = -count * 2;
+    const circles = count > 0 ? this.renderCircles(count - 1) : '';
+    const styles = count === NUM_SPINNERS ? { marginTop: margin } : {};
+
+    return <div className="circle" style={ styles }>{ circles }</div>;
+  };
+
   render = () => {
     return (
       <div className="circle-app">
-        <div className="circle">
-          <div className="circle">
-            <div className="circle">
-              <div className="circle">
-                <div className="circle">
-                  <div className="circle">
-                    <div className="circle">
-                      <div className="circle">
-                        <div className="circle">
-                          <div className="circle">
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        { this.renderCircles(NUM_SPINNERS) }
       </div>
     );
   };
